ae3269f35415e7677d1bec1840a0a4f5bb5f3d4e
Guilhem Berna <guilhem@sudokeys.com>
2020-03-10 16:09:22 +0100

    updated for wal-g and apiv3

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,14 @@
-FROM postgres:9.6
+	FROM postgres:9.6
 
-RUN apt-get update && apt-get -yq install sudo daemontools  pv python3-pip lzop && python3 -m pip install wal-e[swift] \
+RUN apt-get update && apt-get -yq install curl sudo daemontools  pv python3-pip lzop && python3 -m pip install wal-e[swift] \
     && apt-get autoclean
 
+ENV WALG_VERSION="v0.2.14"
 ADD pgbkp.sh /sudokeys/
 
 RUN ln -fs /usr/share/zoneinfo/Europe/Paris /etc/localtime && dpkg-reconfigure -f noninteractive tzdata
 RUN localedef -i fr_FR -c -f UTF-8 -A /usr/share/locale/locale.alias fr_FR.UTF-8
+
+
+
+RUN cd /usr/local/bin && curl -L https://github.com/wal-g/wal-g/releases/download/$WALG_VERSION/wal-g.linux-amd64.tar.gz | tar xzf -
--- pgbkp.sh
+++ pgbkp.sh
@@ -3,31 +3,31 @@
 
 TIMEOUT="${1:-1800}"
 
-WALE_WAL_CONTAINER=$(grep -Po '^[^#].*WALE_SWIFT_PREFIX="\K[^"]*' /var/lib/postgresql/data/postgresql.conf)
+WALG_WAL_CONTAINER=$(grep -Po '^[^#].*WALG_SWIFT_PREFIX="\K[^"]*' /var/lib/postgresql/data/postgresql.conf)
 result=$?
 
 if [[ $result -eq 0 ]]
 then
 
-	sudo -u postgres SWIFT_REGION=GRA3 SWIFT_AUTHURL=https://auth.cloud.ovh.net/v2.0/ WALE_SWIFT_PREFIX=$WALE_WAL_CONTAINER timeout $TIMEOUT ionice -c2 -n7 nice -n19 envdir /run/secrets/ wal-e backup-push $PGDATA
+	sudo -u postgres SWIFT_REGION=GRA SWIFT_AUTHURL=https://auth.cloud.ovh.net/v3/ WALG_SWIFT_PREFIX=$WALG_WAL_CONTAINER timeout $TIMEOUT ionice -c2 -n7 nice -n19 envdir /run/secrets/ wal-g backup-push $PGDATA
 	result=$?
 
 	if [[ $result -eq 0 ]]
 	then
-	        echo "Backup to $WALE_WAL_CONTAINER successful"
+	        echo "Backup to $WALG_WAL_CONTAINER successful"
 	else
-	        >&2 echo  "Backup to $WALE_WAL_CONTAINER failed"
+	        >&2 echo  "Backup to $WALG_WAL_CONTAINER failed"
 		exit $result
 	fi
 
-	sudo -u postgres SWIFT_REGION=GRA3 SWIFT_AUTHURL=https://auth.cloud.ovh.net/v2.0/ WALE_SWIFT_PREFIX=$WALE_WAL_CONTAINER timeout $TIMEOUT ionice -c2 -n7 nice -n19 envdir /run/secrets/ wal-e delete --confirm retain 15
+	sudo -u postgres SWIFT_REGION=GRA SWIFT_AUTHURL=https://auth.cloud.ovh.net/v3/ WALG_SWIFT_PREFIX=$WALG_WAL_CONTAINER timeout $TIMEOUT ionice -c2 -n7 nice -n19 envdir /run/secrets/ wal-g delete --confirm retain 15
 	result=$?
 
 	if [[ $result -eq 0 ]]
 	then
-	        echo "Cleaning of $WALE_WAL_CONTAINER successful"
+	        echo "Cleaning of $WALG_WAL_CONTAINER successful"
 	else
-	        >&2 echo  "Cleaning of $WALE_WAL_CONTAINER failed"
+	        >&2 echo  "Cleaning of $WALG_WAL_CONTAINER failed"
 		exit $result
 	fi
 fi
