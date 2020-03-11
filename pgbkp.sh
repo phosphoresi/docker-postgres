b2d4609f630978a979853ac95b2cde03447b8d7e
Guilhem Berna <guilhem@sudokeys.com>
2020-03-11 15:12:35 +0100

    fix backup script for walg

--- pgbkp.sh
+++ pgbkp.sh
@@ -1,33 +1,24 @@
 #!/bin/bash
 
-
+parameters=$(grep archive_command /var/lib/postgresql/data/conf.d/walg.conf | cut -d '=' -f 2-  | sed -e 's/wal-g wal-push.*//g' | sed -e "s/'//g")
 TIMEOUT="${1:-1800}"
 
-WALG_WAL_CONTAINER=$(grep -Po '^[^#].*WALG_SWIFT_PREFIX="\K[^"]*' /var/lib/postgresql/data/postgresql.conf)
+eval "$parameters" timeout $TIMEOUT ionice -c2 -n7 nice -n19 wal-g backup-push $PGDATA
 result=$?
 
-if [[ $result -eq 0 ]]
-then
+if [[ $result -eq 0 ]];then
+  echo "Backup to $WALG_SWIFT_PREFIX successful"
+else
+  >&2 echo  "Backup to $WALG_SWIFT_PREFIX failed"
+  exit $result
+fi
 
-	sudo -u postgres SWIFT_REGION=GRA SWIFT_AUTHURL=https://auth.cloud.ovh.net/v3/ WALG_SWIFT_PREFIX=$WALG_WAL_CONTAINER timeout $TIMEOUT ionice -c2 -n7 nice -n19 envdir /run/secrets/ wal-g backup-push $PGDATA
-	result=$?
+eval "$parameters" timeout $TIMEOUT ionice -c2 -n7 nice -n19 wal-g delete --confirm retain 15
+result=$?
 
-	if [[ $result -eq 0 ]]
-	then
-	        echo "Backup to $WALG_WAL_CONTAINER successful"
-	else
-	        >&2 echo  "Backup to $WALG_WAL_CONTAINER failed"
-		exit $result
-	fi
-
-	sudo -u postgres SWIFT_REGION=GRA SWIFT_AUTHURL=https://auth.cloud.ovh.net/v3/ WALG_SWIFT_PREFIX=$WALG_WAL_CONTAINER timeout $TIMEOUT ionice -c2 -n7 nice -n19 envdir /run/secrets/ wal-g delete --confirm retain 15
-	result=$?
-
-	if [[ $result -eq 0 ]]
-	then
-	        echo "Cleaning of $WALG_WAL_CONTAINER successful"
-	else
-	        >&2 echo  "Cleaning of $WALG_WAL_CONTAINER failed"
-		exit $result
-	fi
+if [[ $result -eq 0 ]];then
+  echo "Cleaning of $WALG_SWIFT_PREFIX successful"
+else
+  >&2 echo  "Cleaning of $WALG_SWIFT_PREFIX failed"
+  exit $result
 fi
